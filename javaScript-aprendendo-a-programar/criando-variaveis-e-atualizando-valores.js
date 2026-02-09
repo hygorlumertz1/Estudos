16ef06ba5fa252a433364e02d92a46646b0366f7
hygorlumertz1 <hygor.lumertz@edu.pucrs.br>
2026-02-09 20:50:16 -0300

    estudos

---
+++ javaScript-aprendendo-a-programar/criando-variaveis-e-atualizando-valores.js
@@ -0,0 +1,30 @@
+console.log('Olá, mundo do node.JS!');
+
+// Criando e atribuindo valores a variáveis
+
+idade = 25;
+
+console.log('A minha idade é: ', idade);
+
+// Atualizando o valor da variável
+
+idade = 26;
+
+console.log('Agora minha idade é: ', idade);
+
+// Regras para nomeacao de variaveis
+
+// Não podemos iniciar o nome de uma variável com um número
+// 1nome = 'Hygor'; // Isso é um erro
+
+// Podemos usar um numero depois do primeiro caractere
+
+// Não podemos usar sinais de pontuação, exceto o underline (_)
+
+// Se quiser guardar a data do aniversário, podemos usar a variável data_de_aniversario ou dataDeAniversario, mas não podemos usar data-de-aniversario, pois o sinal de menos é um operador de subtração e isso causaria um erro.
+
+// Variáveis são case-sensitive, ou seja, idade e Idade são variáveis diferentes
+
+dataAniversario = '18 de Setembro';
+
+console.log('Minha data de aniversário é: ', dataAniversario);
